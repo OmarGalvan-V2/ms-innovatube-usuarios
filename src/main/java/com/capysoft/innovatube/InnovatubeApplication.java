b6c11f927be6c17ebcd7d3ddfa3767d5b8a36a0f
OmarGalvan-V2 <omar_galvan@outlook.es>
2025-11-20 01:34:57 -0600

    fix: se arregla LoadBalance

--- src/main/java/com/capysoft/innovatube/InnovatubeApplication.java
+++ src/main/java/com/capysoft/innovatube/InnovatubeApplication.java
@@ -2,21 +2,12 @@ package com.capysoft.innovatube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class InnovatubeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnovatubeApplication.class, args);
-	}
-	
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
 	}
 
 }
